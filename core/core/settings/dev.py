0707cbe3b42a5d77a2596e7ac61031ad40f1868c
dbpython <33459750+papismurf@users.noreply.github.com>
2022-05-31 23:02:18 +0100

    Initial commit

---
+++ core/core/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-zu8x%9y@xa@axm(9to*!ryo-k3js#c_4$=09)lp)8^+0d@1bog"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
